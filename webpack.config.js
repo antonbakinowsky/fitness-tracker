0eb14cc33836cb20c59b0757fb200f1b3e0b1a20
Anton Bakinowsky <antonbakinowsky@gmail.com>
2022-07-02 15:29:22 +0400

    init projects

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.tsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+	},
+	optimization: {
+		runtimeChunk: 'single',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	plugins: [new HtmlWebpackPlugin()],
+};
